3c9b943cb00415eb1b33401619a054ac6f27a470
yanxia97 <isaacyan@umich.edu>
2018-09-19 18:14:47 -0400

    Committed by isaacyan

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,16 @@
-def question():
-	question = input(“What is your question?”)
+def ask_question():
+	question = input("What is your question?\n")
+	return question
+# edited by isaacyan
+
+
+if __name__ == "__main__":
+	while True:
+		question = ask_question()
+		if question == "exit":
+			break
+		if question[-1]!='?':
+			print("I’m sorry, I can only answer questions")
+		else:
+			break
 # edited by isaacyan
